5aa025c761f7e5688a70ed75d44c8662f21fa7b9
jagpreetrahi <jagpreet1615@gmail.com>
2025-04-05 18:02:04 +0530

    Contain the raw query

---
+++ src/repositories/airplane-repositories.js
@@ -0,0 +1,11 @@
+const CrudRepository = require('./crud_repository')
+const {Airplane} = require('../models')
+
+class AirplaneRepository extends CrudRepository{
+
+    constructor(){
+      super(Airplane)
+    }
+}
+
+module.exports = AirplaneRepository;
