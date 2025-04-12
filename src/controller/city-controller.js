8bc4cdba622bd444cdfb1db313a924b95df4bef6
jagpreetrahi <jagpreet1615@gmail.com>
2025-04-12 15:17:39 +0530

    Added the city controller code

---
+++ src/controller/city-controller.js
@@ -0,0 +1,88 @@
+const {CityService} = require('../services')
+const {StatusCodes} = require('http-status-codes')
+const {SuccessResponse , ErrorResponse} = require('../utils/common/index')
+
+async function createCity(req , res) {
+
+    try {
+        const city = await CityService.createCity({
+            name : req.body.name
+        })
+        SuccessResponse.data = city
+        return res.status(StatusCodes.CREATED).json(SuccessResponse)
+    } catch (error) {
+        ErrorResponse.error = error;
+        return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse)
+    }
+}
+
+async function getCity(req, res){
+   
+    try {
+        
+        const city = await CityService.getCity(req.params.id);
+        
+        SuccessResponse.data = city;
+        return res.status(StatusCodes.OK).json(SuccessResponse)
+    } catch (error) {
+         ErrorResponse.error  = error;
+         return res 
+                 .status(error.statusCode)
+                 .json(ErrorResponse)
+    }
+}
+
+async function getCities(req, res){
+   
+    try {
+        
+        const cities = await CityService.getCities();
+        
+        SuccessResponse.data = cities;
+        return res.status(StatusCodes.OK).json(SuccessResponse)
+    } catch (error) {
+         ErrorResponse.error  = error;
+         return res 
+                 .status(error.statusCode)
+                 .json(ErrorResponse)
+    }
+}
+
+async function destroyCity(req, res){
+   
+    try {
+        
+        const city = await CityService.destroyCity(req.params.id);
+        
+        SuccessResponse.data = city;
+        return res.status(StatusCodes.OK).json(SuccessResponse)
+    } catch (error) {
+         ErrorResponse.error  = error;
+         return res 
+                 .status(error.statusCode)
+                 .json(ErrorResponse)
+    }
+}
+
+async function updateCity(req, res){ 
+   
+    try {
+        
+        const city = await CityService.updateCity({name : req.body.name} , req.params.id);
+        SuccessResponse.data = city;
+        return res.status(StatusCodes.OK).json(SuccessResponse)
+    } catch (error) {
+         ErrorResponse.error  = error;
+         return res 
+                 .status(error.statusCode)
+                 .json(ErrorResponse)
+    }
+}
+
+module.exports = {
+    createCity,
+    getCities,
+    getCity,
+    destroyCity,
+    updateCity
+}
