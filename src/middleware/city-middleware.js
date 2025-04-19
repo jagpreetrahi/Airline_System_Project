25d177128bdb14d33dd64addc733bc1f1baa6862
jagpreetrahi <jagpreet1615@gmail.com>
2025-04-19 22:33:30 +0530

    Fix: Changes static message by  variable

--- src/middleware/city-middleware.js
+++ src/middleware/city-middleware.js
@@ -4,8 +4,9 @@ const {StatusCodes} = require('http-status-codes');
 const validateCreateCity = (req, res ,next) => {
   
     const name = req.body.name;
+    const message = "Something went wrong while creating a city";
     if(!name){
-        ErrorResponse.message = "Something went wrong while creating a city"
+        ErrorResponse.message = message;
         ErrorResponse.error = {explanation :  "You are not providing the correct details"}
         return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
 
